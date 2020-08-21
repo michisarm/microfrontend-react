13f7514f3bc78fade6d9d0f89022f1f4c9276b13
michisarm <michisarm@gmail.com>
2020-08-21 17:05:45 +0900

    backend 추가

---
+++ backend/src/test/java/com/microfrontend/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microfrontend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
