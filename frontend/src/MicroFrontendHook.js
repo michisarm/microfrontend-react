01ad15a03aa6cd7da2489b797e7e6e4088916222
미치삶 <michisarm@gmail.com>
2020-08-25 07:02:40 +0900

    production ing..

--- frontend/src/MicroFrontendHook.js
+++ frontend/src/MicroFrontendHook.js
@@ -16,12 +16,44 @@ const MicroFrontendHook = ({ name, host, history }) => {
     fetch(`${host}/asset-manifest.json`)
       .then(res => res.json())
       .then(manifest => {
+        //css
+        // const promisesCss = Object.keys(manifest['files'])
+        //   .filter(key => key.endsWith('.css'))
+        //   .reduce((sum, key) => {
+        //     sum.push(
+        //       new Promise(resolve => {
+        //         let path;
+        //         if (host.indexOf('localhost') > -1){
+        //           path = `${host}${manifest['files'][key]}`;
+        //         }else{
+        //           path = `${manifest['files'][key]}`;
+        //         }
+        //         const link = document.createElement('link');
+        //         if (key === 'main.css') {
+        //           link.id = scriptId+"-CSS";
+        //         }
+        //         link.onload = () => {
+        //           resolve();
+        //         };
+        //         link.href = path;
+        //         link.ref = 'stylesheet';
+        //         document.head.appendChild(link);
+        //       })
+        //     );
+        //     return sum;
+        //   }, []);
+        //js
         const promises = Object.keys(manifest['files'])
           .filter(key => key.endsWith('.js'))
           .reduce((sum, key) => {
             sum.push(
               new Promise(resolve => {
-                const path = `${host}${manifest['files'][key]}`;
+                let path;
+                if (host.indexOf('localhost') > -1){
+                  path = `${host}${manifest['files'][key]}`;
+                }else{
+                  path = `${manifest['files'][key]}`;
+                }
                 const script = document.createElement('script');
                 if (key === 'main.js') {
                   script.id = scriptId;
@@ -35,6 +67,8 @@ const MicroFrontendHook = ({ name, host, history }) => {
             );
             return sum;
           }, []);
+        
+        // const promises = [promisesCss, promisesJs];
         Promise.allSettled(promises).then(() => {
           renderMicroFrontend();
         });
